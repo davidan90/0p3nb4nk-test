450ccc1f37d0a671f0ad1e10892a24497078fc7c
David Alvarez <david16an@gmail.com>
2020-10-29 15:19:58 +0100

    Updates

---
+++ precache-manifest.fd75cd73b2e9b92ffb7bcb2f8fb29712.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1c29abd0a3ac68cf4cc5a84d3057525",
+    "url": "/0p3nb4nk-test/index.html"
+  },
+  {
+    "revision": "51977e935537ec8fa715",
+    "url": "/0p3nb4nk-test/static/css/main.345d2372.chunk.css"
+  },
+  {
+    "revision": "1fc6b7ce4b9ca7d6322c",
+    "url": "/0p3nb4nk-test/static/js/2.e226bdef.chunk.js"
+  },
+  {
+    "revision": "ee9d6ff43844efbe4ddeab3c8d2b69b8",
+    "url": "/0p3nb4nk-test/static/js/2.e226bdef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51977e935537ec8fa715",
+    "url": "/0p3nb4nk-test/static/js/main.12b16c8b.chunk.js"
+  },
+  {
+    "revision": "327d8d4fd2d864f43fb1",
+    "url": "/0p3nb4nk-test/static/js/runtime-main.d502adcb.js"
+  },
+  {
+    "revision": "828e06a5791d1c1cb89cd6cd7da78b3a",
+    "url": "/0p3nb4nk-test/static/media/secure-1.828e06a5.svg"
+  },
+  {
+    "revision": "31499ccb464a326d26f3004f9e48772f",
+    "url": "/0p3nb4nk-test/static/media/secure-2.31499ccb.svg"
+  }
+]);
